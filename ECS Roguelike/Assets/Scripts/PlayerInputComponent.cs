cc8d40d76be418ef07c5ecc18d04ccc1b5503b7b
SwordOfWrynn <bubba.wiens@gmail.com>
2019-05-26 17:59:18 -0700

    Removed the MemoryProfiler package because it was throwing exceptions

---
+++ ECS Roguelike/Assets/Scripts/PlayerInputComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerInputComponent : IComponentData
+{
+    public bool leftClick;
+    public bool rightClick;
+    public float3 mousePosition;
+}
