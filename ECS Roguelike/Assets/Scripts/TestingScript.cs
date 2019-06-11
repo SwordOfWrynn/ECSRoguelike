7506fe5fa39cca43ed842e9af0df3571b1ff71e1
SwordOfWrynn <bubba.wiens@gmail.com>
2019-06-11 09:30:23 -0700

    Changes to TestingScript

--- ECS Roguelike/Assets/Scripts/TestingScript.cs
+++ ECS Roguelike/Assets/Scripts/TestingScript.cs
@@ -15,16 +15,14 @@ using UnityEngine.Networking;
 
 public class TestingScript : MonoBehaviour
 {
-    public GameObject playerSpritePrefab;
+    public GameObject spritePrefab;
     Dictionary<string, Type> componentTypeDictionary = new Dictionary<string, Type>();
 
-    public Sprite[] spritesArray;
-    public int test = 0;
-    GameObject player;
+    List<SpriteSheet> spriteSheets = new List<SpriteSheet>();
+
     // Start is called before the first frame update
     void Start()
     {
-
         var componentDataQuery =
             from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
             from type in domainAssembly.GetLoadableTypes()
@@ -33,7 +31,7 @@ public class TestingScript : MonoBehaviour
             select type;
 
         StringBuilder sb = new StringBuilder();
-        foreach(var value in componentDataQuery)
+        foreach (var value in componentDataQuery)
         {
             sb.Append($"{value.Name}, ");
             componentTypeDictionary.Add(value.Name, value);
@@ -50,33 +48,17 @@ public class TestingScript : MonoBehaviour
             );
 
         Entity entity = EntityManager.CreateEntity(playerArchtype);
-        GameObject gameObject = Instantiate(playerSpritePrefab);
+        GameObject gameObject = Instantiate(spritePrefab);
 
-        FileInfo spriteFile = new FileInfo(Application.streamingAssetsPath + "/" + unit.SpritePath);
-
-        //StartCoroutine(GetTextureAsync(spriteFile));
-
-        byte[] imageData = File.ReadAllBytes(spriteFile.FullName);
-        Texture2D texture = new Texture2D(2, 2);
-        texture.LoadImage(imageData);
-        texture.filterMode = FilterMode.Point;
-        int arraySize = (texture.width / 16) * (texture.height / 16);
-        Sprite[] sprites = new Sprite[arraySize];
-        int arrayPosition = 0;
-        for (int x = 0; x < texture.width/16; x++)
+        int spriteSheetIndex = CheckSpriteSheetsForMatch(spriteSheets, unit.SpritePath);
+        if (spriteSheetIndex == -1)
         {
-            for (int y = 0; y < texture.height/16; y++)
-            {
-                Debug.Log("At position " + x + "," + y);
-                sprites[arrayPosition] = Sprite.Create(texture, new Rect(x * 16, y * 16, 16, 16), new Vector2(0.5f, 0.5f), 16);
-                //sprites[arrayPosition].
-                arrayPosition++;
-            }
+            FileInfo spriteFile = new FileInfo(Application.streamingAssetsPath + "/" + unit.SpritePath);
+            spriteSheets.Add(new SpriteSheet() { Name = unit.SpritePath, Sprites = GetSpriteSheetFromFile(spriteFile) });
+            spriteSheetIndex = spriteSheets.Count - 1;
         }
-        spritesArray = sprites;
 
-        //Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 16);
-        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[unit.SpritePosition];
+        gameObject.GetComponent<SpriteRenderer>().sprite = spriteSheets[spriteSheetIndex].Sprites[unit.SpritePosition];
 
         int visualID = SpriteManager.RegisterVisualGameObjectAndReturnKey(gameObject);
 
@@ -84,6 +66,14 @@ public class TestingScript : MonoBehaviour
         {
             VisualID = visualID
         });
+
+        var staminaIndex = unit.Components.Select((value, index) => new { value, index = index + 1 })
+                .Where(pair => pair.value.Name == "Stamina")
+                .Select(pair => pair.index)
+                .FirstOrDefault() - 1;
+
+        Debug.Log($"The stamina to start is {unit.Components[staminaIndex].Values[0]}");
+
         EntityManager.SetComponentData(entity, new Stamina
         {
             Value = 5,
@@ -111,26 +101,67 @@ public class TestingScript : MonoBehaviour
             componentTypes[i] = componentTypeDictionary[unit.Components[i].Name];
         }
 
-        return componentTypes.Where(c => c!= null).ToArray();
+        return componentTypes.Where(c => c != null).ToArray();
     }
 
-    IEnumerator GetTextureAsync(FileInfo spriteFile)
+    public Sprite[] GetSpriteSheetFromFile(FileInfo spriteFile)
     {
-        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(@"file:///" + spriteFile.FullName.ToString()))
+        byte[] imageData = File.ReadAllBytes(spriteFile.FullName);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(imageData);
+        texture.filterMode = FilterMode.Point;
+        int arraySize = (texture.width / 16) * (texture.height / 16);
+        Sprite[] sprites = new Sprite[arraySize];
+        int arrayPosition = 0;
+        for (int x = 0; x < texture.width / 16; x++)
         {
-            yield return request.SendWebRequest();
-
-            if (request.isNetworkError || request.isHttpError)
+            for (int y = 0; y < texture.height / 16; y++)
             {
-                Debug.Log(request.error);
+                //Debug.Log("At position " + x + "," + y);
+                sprites[arrayPosition] = Sprite.Create(texture, new Rect(x * 16, y * 16, 16, 16), new Vector2(0.5f, 0.5f), 16);
+                //sprites[arrayPosition].
+                arrayPosition++;
             }
-            else
-            {
-                // Get downloaded asset bundle
-                var texture = DownloadHandlerTexture.GetContent(request);
-            }
-
         }
+        return sprites;
+    }
+
+    /// <summary>
+    /// Checks the give array for a name match, and returns the index of the match if found, and a -1 if no match was found
+    /// </summary>
+    /// <param name="spriteSheetList"></param>
+    /// <param name="spriteSheetName"></param>
+    /// <returns></returns>
+    int CheckSpriteSheetsForMatch(List<SpriteSheet> spriteSheetList, string spriteSheetName)
+    {
+        return  spriteSheetList.Select((value, index) => new { value, index = index + 1 })
+                .Where(pair => pair.value.Name == spriteSheetName)
+                .Select(pair => pair.index)
+                .FirstOrDefault() - 1;
+    }
+
+    //IEnumerator GetTextureAsync(FileInfo spriteFile)
+    //{
+    //    using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(@"file:///" + spriteFile.FullName.ToString()))
+    //    {
+    //        yield return request.SendWebRequest();
+
+    //        if (request.isNetworkError || request.isHttpError)
+    //        {
+    //            Debug.Log(request.error);
+    //        }
+    //        else
+    //        {
+    //            // Get downloaded asset bundle
+    //            var texture = DownloadHandlerTexture.GetContent(request);
+    //        }
+
+    //    }
+    //}
+
+    struct SpriteSheet {
+        public string Name;
+        public Sprite[] Sprites;
     }
 
 }
